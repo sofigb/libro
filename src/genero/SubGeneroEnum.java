958af2e8e6d1a566e8c0ae16760dc98b7d0c987c
Mirna Pamela Diaz <pamela_diaz_24@outlook.com>
2021-09-30 15:05:57 -0300

    Agregacion de la clase Enum

---
+++ src/genero/SubGeneroEnum.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genero;
+
+/**
+ *
+ * @author Mirna
+ */
+public enum SubGeneroEnum {
+    EGOGLA,ELEGIA,HIMNO,ODA,SATIRA,EPIGRAMA,MADRIGAL,NOVELA,CUENTO,NOVELA_CORTA,LEYENDA,EPOPEYA,POEMA_EPICO,
+    TRAGEDIA,DRAMA,COMEDIA,AUTO_sACRAMENTAL,SAINETTE,PASO,ENTREMES,ENSAYO,FABULA,EPISTOLA,DIALOGO;
+    
+}
